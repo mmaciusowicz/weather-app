10ec816a7c23827208015b041e637e2821130286
Marek Maciusowicz <mmaciusowicz@users.noreply.github.com>
2018-09-10 18:55:19 +0100

    Implement data retrieval and source validation checks for PullWeatherDataCommand

--- src/Command/PullWeatherDataCommand.php
+++ src/Command/PullWeatherDataCommand.php
@@ -23,6 +23,39 @@ class PullWeatherDataCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $source_url = $input->getArgument('source_url');
 
+        $source_data = $this->retrieveDataFromSource($source_url);
+    }
+
+    protected function retrieveDataFromSource($source_url) {
+        if (filter_var($source_url, FILTER_VALIDATE_URL) === FALSE) {
+            throw new \Error('Invalid source url');
+        }
+
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $source_url);
+
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        // Execute request.
+        $source_data = curl_exec($ch);
+
+        // Check for errors.
+        if ($errno = curl_errno($ch)) {
+            throw new \Error(curl_strerror($errno));
+        }
+
+        // Check response code.
+        $response_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+        if ($response_code >= 400) {
+            throw new \Error('Request to source failed with code: ' . $response_code);
+        }
+
+        curl_close($ch);
+
+        return $source_data;
     }
 }
