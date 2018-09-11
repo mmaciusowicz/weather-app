7bf272426746c58d8d9ff934282f768093d1eef0
Marek Maciusowicz <mmaciusowicz@users.noreply.github.com>
2018-09-11 18:31:13 +0100

    Add a test for successful execution of PullWeatherDataCommand

--- tests/Command/PullWeatherDataCommandTest.php
+++ tests/Command/PullWeatherDataCommandTest.php
@@ -16,6 +16,42 @@ class PullWeatherDataCommandTest extends KernelTestCase {
      */
     private $weatherRecordManager;
 
+    public function testExecute() {
+        $date_yesterday = (new \DateTime('now -1 day'))->format("Y-m-d");
+
+        $mock_data_retriever = $this->createMock(DataRetriever::class);
+
+        $mock_data_retriever->expects($this->any())
+            ->method('retrieve')
+            ->willReturn('{"date": "' . $date_yesterday . '", "temperature": 19, "chance_for_rain": 84}');
+
+        $kernel = static::createKernel();
+
+        $kernel->boot();
+
+        $application = new Application($kernel);
+
+        $application->add(new PullWeatherDataCommand($mock_data_retriever, $this->weatherRecordManager));
+
+        $command = $application->find('app:pull-weather-data');
+
+        $command_tester = new CommandTester($command);
+
+        $command_tester->execute(array(
+            'command'  => $command->getName(),
+            'source_url' => 'http://localhost/mock-source',
+        ));
+
+        $this->assertContains('Created a record for date ' . $date_yesterday . '.', $command_tester->getDisplay());
+
+        $command_tester->execute(array(
+            'command'  => $command->getName(),
+            'source_url' => 'http://localhost/mock-source',
+        ));
+
+        $this->assertContains('Record for date ' . $date_yesterday . ' already exists, check later.', $command_tester->getDisplay());
+    }
+
     public function testParseSourceData() {
         $parse_source_data = self::getReflectionMethod('App\Command\PullWeatherDataCommand', 'parseSourceData');
 
@@ -158,6 +194,13 @@ class PullWeatherDataCommandTest extends KernelTestCase {
         $kernel = self::bootKernel();
 
         $container = $kernel->getContainer();
+
+        // Ensure no records are in database before testing.
+        $container
+            ->get('doctrine')
+            ->getManager()
+            ->createQuery('DELETE FROM \App\Entity\WeatherRecord')
+            ->execute();
 
         // Define weatherRecordManager.
         $this->weatherRecordManager = new WeatherRecordManager($container
